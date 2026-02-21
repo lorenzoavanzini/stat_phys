f5478433a706c4c3da2bc9cef2f7a94c947d7b5d
Lorenzo Avanzini <lorenzo.avanzini0@gmail.com>
2026-02-21 01:06:58 +0100

    Added metadata to __init__.py
    Added docstring, version, author, license and __all__

---
+++ stat_phys/__init__.py
@@ -0,0 +1,11 @@
+"""
+stat_phys: a Python library for performing statistical physics simulations.
+
+Modules:
+- canonical: Canonical ensemble simulations
+- microcanonical: Microcanonical ensemble simulations
+"""
+version = '0.1'
+author = 'Lorenzo Avanzini'
+license = 'MIT'
+__all__ = ['CanonicalMC', 'version', 'author', 'license']
