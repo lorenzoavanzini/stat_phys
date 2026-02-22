eaffacd532b394911e71cd61bda113a9789dad97
Lorenzo Avanzini <lorenzo.avanzini0@gmail.com>
2026-02-22 14:13:58 +0100

    Fix formatting of module docstring in __init__.py

--- stat_phys/__init__.py
+++ stat_phys/__init__.py
@@ -4,11 +4,14 @@ stat_phys: a Python library for performing statistical physics simulations.
 Modules:
 - canonical: Canonical ensemble simulations
 """
+
 version = '0.1.0'
 author = 'Lorenzo Avanzini'
 license = 'MIT'
 modules = ['canonical']
+
 __all__ = ['CanonicalMC', 'version', 'author', 'license']
+
 
 
 
