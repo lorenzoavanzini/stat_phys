c3ecebd69ed456143da387f056beafb7922e1fe8
Lorenzo Avanzini <lorenzo.avanzini0@gmail.com>
2026-02-22 01:31:14 +0100

    Update module docstring and version number

--- stat_phys/__init__.py
+++ stat_phys/__init__.py
@@ -5,7 +5,8 @@ Modules:
 - canonical: Canonical ensemble simulations
 - microcanonical: Microcanonical ensemble simulations
 """
-version = '0.1'
+version = '0.1.0'
 author = 'Lorenzo Avanzini'
 license = 'MIT'
 __all__ = ['CanonicalMC', 'version', 'author', 'license']
+
