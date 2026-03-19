87713bc7785136aefaa833d8fc68f5f921ed38dc
Lorenzo Avanzini <lorenzo.avanzini0@gmail.com>
2026-03-19 21:50:06 +0100

    v0.3.0

--- stat_phys/ensembles/canonical.py
+++ stat_phys/ensembles/canonical.py
@@ -28,11 +28,36 @@ class CanonicalMC:
             raise ValueError("Temperature not set!")
         return 1/(self.kb*self.T)
 
-    def metropolis(self, steps, thermal_steps, progress = True):
-        observables = np.zeros([thermal_steps+steps, self.system.n_obs])
-        iterator = range(thermal_steps+steps)
+    def _C(self, tau, x):
+        N = x.shape[0]
+        if tau >= N:
+            return 0.0
+        mean = x.mean()
+        xy = np.mean(x[:N-tau]*x[tau:])
+        return xy-mean**2
+
+    def _rho(self, tau, x, cutoff = 1e-4):
+        Ctau = self._C(tau, x)
+        C0 = self._C(0, x)
+        if abs(C0) < cutoff:
+            return 0.0
+        return Ctau/C0
+        
+    def _autocorr_time(self, chain, c = 5, cutoff = 1e-5):
+        t = 0
+        tau = self._rho(t, chain, cutoff)
+        while t < c*tau and t < len(chain)//2:
+            t += 1
+            rho_t = self._rho(t, chain, cutoff)
+            tau += rho_t
+        return tau
+
+    def metropolis(self, steps, thermal_steps, thinning = 1, correlation = True, c = 5, cutoff = 1e-5, progress = True):
+        observables = np.zeros([steps, self.system.n_obs])
+        total_steps = thermal_steps+steps*thinning
+        iterator = range(total_steps)
         if progress:
-            iterator = tqdm(iterator, desc = 'MC steps', leave = False)
+            iterator = tqdm(iterator, desc = 'MCMC steps', leave = False)
 
         accepted = 0
         for step in iterator:
@@ -42,72 +67,51 @@ class CanonicalMC:
             if dE <= 0 or np.random.rand() < np.exp(-self.beta*dE):
                 accepted += 1
                 self.system._accept_move(new_state = new_state, new_energy = E_new)
- 
-            observables[step, :] = self.system.observables
-        if steps!=0:
-            acceptance_rate = accepted/(steps)
+
+            if step >= thermal_steps and (step-thermal_steps)%thinning == 0:
+                observables[(step-thermal_steps)//thinning, :] = self.system.observables
+        if steps != 0:
+            acceptance_rate = accepted/(steps*thinning)
         else:
             acceptance_rate = None
-        return observables[thermal_steps:,:], acceptance_rate
 
-    def run(self, Tstart, Tend, n_T, n_sample, thermal_steps, reset = False, description = 'Temperature sweep', thermal_each_T = False):
+        if correlation == True:
+            autocorr_time = np.zeros(self.system.n_obs)
+            for i in range(self.system.n_obs):
+                autocorr_time[i] = self._autocorr_time(observables[:, i], c, cutoff)
+        else:
+            autocorr_time = None
+        return observables, autocorr_time, acceptance_rate
+
+    def sweep(self, Tstart, Tend, n_T, n_sample, thermal_steps, thinning = 1, correlation = True, c = 5, cutoff = 1e-5, 
+            reset = False, progress = True, description = 'Temperature sweep', thermal_each_T = False):
+        
         T = np.linspace(Tstart, Tend, n_T)
         self.set_temperature(Tstart)
         dimensions = [T.shape[0], n_sample, self.system.n_obs]
+        DIMENSIONS = [T.shape[0], self.system.n_obs]
         observables = np.zeros(dimensions)
+        autocorr_times = np.zeros(DIMENSIONS)
         acceptance_rates = np.zeros(n_T)
 
         if reset:
             self.system.state_reset()
-        _, _ = self.metropolis(steps = 0, thermal_steps = thermal_steps)
+        _, _, _ = self.metropolis(steps = 0, thermal_steps = thermal_steps, correlation = False)
 
-        for i in tqdm(range(T.shape[0]), desc = description):
+        iterator = range(T.shape[0])
+        if progress:
+            iterator = tqdm(iterator, desc = description, leave = False)
+            
+        for i in iterator:
             self.set_temperature(T[i])
             if not thermal_each_T:
                 loop_thermal_steps = 0
             else:
                 loop_thermal_steps = thermal_steps
-            observables[i,:,:], acceptance_rates[i] = self.metropolis(steps = n_sample, thermal_steps = loop_thermal_steps)
+            observables[i,:,:], autocorr_times[i,:], acceptance_rates[i] = self.metropolis(steps = n_sample, thermal_steps = loop_thermal_steps, 
+                                                                                           thinning = thinning, correlation = correlation, c = c, 
+                                                                                           cutoff = cutoff, progress = progress)
 
-        return observables, acceptance_rates #n_T, n_sample, n_obs/ n_T
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        return observables, autocorr_times, acceptance_rates #n_T, n_sample, n_obs/ n_T, n_obs / n_T
 
 
